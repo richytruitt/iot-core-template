4318a63a18a14738991ccae348e5aa0f8945d381
richytruitt <richyctruitt@gmail.com>
2026-08-13 16:01:13 -0500

    cleaned up unneeded outputs

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "device_certificates" {
-  value = module.register_things.device_credentials
-  sensitive = true
-}
-
-output "root_ca_certificate" {
-    value = module.custom_root_ca.root_ca_certificate
-    sensitive = true
-}
